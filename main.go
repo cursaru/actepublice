fd4ae4622905af97e68c6f4c869d97f3825e93fa
flaviu <flaviu@DESKTOP-T814V00>
2022-11-06 16:42:48 +0200

    add bootstrap and folders

--- main.go
+++ main.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
-	
+	"net/http"
 )
 
 func main() {
 
-	var teststring = "Functioneaza"
-	fmt.Printf(teststring)
+	const numarport string = ":7000"
+	contor := 0
+
+	fmt.Printf("Starting...")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+
+		x, err := fmt.Fprintf(w, "ActePublice.Ro %d", contor)
+
+		if err != nil {
+
+			fmt.Println(err)
+		} else {
+			fmt.Println(x)
+		}
+		contor ++
+	})
+
+	fs := http.FileServer(http.Dir("static/"))
+
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	_ = http.ListenAndServe(numarport, nil)
 
 }
