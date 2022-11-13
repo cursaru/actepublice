1e4f38c95733090c7cf4296322f72bfc1112f53a
flaviu <flaviu@DESKTOP-T814V00>
2022-11-13 18:49:38 +0200

    changes

---
+++ renders/render.go
@@ -0,0 +1,73 @@
+package renders
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+
+
+var tc = make(map[string]*template.Template)
+
+func RenderTemplate(w http.ResponseWriter, t string, data interface{}) {
+
+	var tmpl *template.Template
+	var err error
+
+	//verificam daca exista template-ul in tc
+	// inMap este un bool
+	_, inMap := tc[t]
+
+	if !inMap {
+		// trebuie facut ceva daca nu exista in cache, adica este false
+
+		log.Println("Creating template and adding to cache...")
+		err = createTemplateCache(t)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+	} else {
+		// trebuie facut ceva daca exista in cache, adica este true
+
+		log.Println("serving ", t, " from cache")
+
+		// ca sa vad eu cum e treba si ce e in cache, dar nu e obligatoriu
+		for element, _ := range tc {
+
+			log.Println(element)
+		}
+	}
+
+	tmpl = tc[t]
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Println(err)
+	}
+
+}
+
+func createTemplateCache(t string) error {
+
+	templates := []string{
+
+		fmt.Sprintf("./templates/%s", t),
+		"./templates/base.layout.html",
+	}
+
+	// parse the template
+	tmpl, err := template.ParseFiles(templates...)
+
+	if err != nil {
+
+		return err
+	}
+
+	tc[t] = tmpl
+
+	return nil
+}
