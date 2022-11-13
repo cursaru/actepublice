4e2089f695f70ebda7f105f0edcff43f838897f0
flaviu <flaviu@DESKTOP-T814V00>
2022-11-13 12:31:08 +0200

    changes

--- main.go
+++ main.go
@@ -3,32 +3,70 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
 
 	const numarport string = ":7000"
-	contor := 0
+	//contor := 0
+
+
+	r := mux.NewRouter()
 
 	fmt.Printf("Starting...")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
-		x, err := fmt.Fprintf(w, "ActePublice.Ro %d", contor)
+	// 	x, err := fmt.Fprintf(w, "ActePublice.Ro %d", contor)
 
-		if err != nil {
+	// 	if err != nil {
 
+	// 		fmt.Println(err)
+	// 	} else {
+	// 		fmt.Println(x)
+	// 	}
+	// 	contor ++
+	// })
+
+	r.HandleFunc("/books/{title}/page/{page}", func(w http.ResponseWriter, r *http.Request){
+		
+		parametri := mux.Vars(r)
+
+		fmt.Println(parametri["title"])
+
+		x, err := fmt.Fprintf(w, parametri["title"])
+
+		if err != nil{
 			fmt.Println(err)
 		} else {
 			fmt.Println(x)
 		}
-		contor ++
+
 	})
+
+	r.HandleFunc("/main", func(w http.ResponseWriter, r *http.Request){
+
+		x, err := fmt.Fprintf(w, "pagina main")
+
+		if err != nil{
+			fmt.Println(err)
+		} else {
+			fmt.Println(x)
+		}
+
+	})
+	
+
+
+
 
 	fs := http.FileServer(http.Dir("static/"))
 
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	_ = http.ListenAndServe(numarport, nil)
+	_ = http.ListenAndServe(numarport, r)  // daca se merge pe cale obisnuita aici nu mai este nil ci r
+
 
 }
