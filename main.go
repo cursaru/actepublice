9b4481d118662df3ccad35ff969f2c473cabba2c
Cursaru Flaviu <flaviu.florian.cursaru@gmail.com>
2023-02-20 18:25:46 +0200

    Update main.go

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"actepublice/renders"
 	"fmt"
 	"net/http"
+
 	"github.com/gorilla/mux"
-	"github.com/cursaru/actepublice/renders"
-	"github.com/cursaru/actpublice/structures"
 )
 
 func main() {
@@ -13,52 +13,46 @@ func main() {
 	const numarport string = ":7000"
 	//contor := 0
 
-
 	r := mux.NewRouter()
 
 	fmt.Printf("Starting...")
 
-	r.HandleFunc("/articole/{articol}", func(w http.ResponseWriter, r *http.Request){
-		
+	r.HandleFunc("/articole/{articol}", func(w http.ResponseWriter, r *http.Request) {
+
 		parametri := mux.Vars(r)
 
-		fmt.Println(parametri["articol"])
+		//fmt.Println(parametri["articol"])
 
-		data := structures.EmptyInteger
+		data := parametri // structures.EmptyInteger
 
-		x, err := fmt.Fprintf(w, parametri["articol"])
+		//x, err := fmt.Fprintf(w, parametri["articol"])
 
-		renders.RenderTemplate(w, "post.html", da)
+		renders.RenderTemplate(w, "post.html", data)
 
-		if err != nil{
-			fmt.Println(err)
-		} else {
-			fmt.Println(x)
-		}
+		//if err != nil{
+		//	fmt.Println(err)
+		//} else {
+		//	fmt.Println(x)
+		//}
 
 	})
 
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 
 		x, err := fmt.Fprintf(w, "pagina main")
 
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 		} else {
 			fmt.Println(x)
 		}
 
 	})
-	
 
+	//fs := http.FileServer(http.Dir("static/"))
 
+	//http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-
-	fs := http.FileServer(http.Dir("static/"))
-
-	http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-	_ = http.ListenAndServe(numarport, r)  // daca se merge pe cale obisnuita aici nu mai este nil ci r
-
+	_ = http.ListenAndServe(numarport, r) // daca se merge pe cale obisnuita aici nu mai este nil ci r
 
 }
